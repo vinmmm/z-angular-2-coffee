a10d18909b566fc8d503a0cc8a4581bac4f8aec2
vinmmm <jvinmoore@gmail.com>
2016-08-05 21:42:36 -0400

    removed images

--- app/app.component.js
+++ app/app.component.js
@@ -24,7 +24,7 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: '<img src="img3/1-home.png"><img src="img3/2-Onboarding.png" alt="iphone coffee"><img src="img3/scroll-down.png" alt="scroll icon"><li class="phone"><img src="img3/3-Sign-In.png" alt="iphone coffee"></li><li class="phone"><img src="img3/4-Sign-Up.png" alt="iphone coffee"></li><li class="phone"><img src="img3/5-Vanilla-Latte.png" alt="iphone coffee"></li><li class="phone"><img src="img3/6-Choose-Items.png" alt="iphone coffee"></li><li class="phone"><img src="img3/7-Confirm-Order.png" alt="iphone coffee"></li><li class="phone"><img src="img3/8-Placing-Order.png" alt="iphone coffee"></li><li class="phone"><img src="img3/9-My-Orders.png" alt="iphone coffee"></li>'
+                        template: '<h1>Angular 2 App</h1>'
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
